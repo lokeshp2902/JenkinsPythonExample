6ba8895fd91344821791af344db8129d964b09db
lokeshp2902 <lokeshkumar.p@nference.net>
2021-09-28 10:18:17 +0530

    Deleted one test case

--- unitTests/test_division.py
+++ unitTests/test_division.py
@@ -1,7 +1,7 @@
 from utilities.Division import Division
 import pytest
 
-@pytest.mark.parametrize("numerator, denominator, result",[(1,2,1),(1,0,0)])
+@pytest.mark.parametrize("numerator, denominator, result",[(1,2,0)])
 def test_div(numerator, denominator, result):
     division = Division()
     assert( division.div(numerator, denominator) == result)
