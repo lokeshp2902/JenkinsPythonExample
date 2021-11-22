12c5adf381a6c87c77b6faeed34ae3ff9f144f49
lokeshp2902 <89763951+lokeshp2902@users.noreply.github.com>
2021-11-22 10:39:23 +0530

    Update Addition.py

--- utilities/Addition.py
+++ utilities/Addition.py
@@ -1,12 +1,12 @@
 class Addition:
 
     def add(self, operand1, operand2):
-        return operand1 * operand2
+        return operand1 + operand2
 
     def addAll(self, numbers):
         sum = 0
         for number in numbers:
-            sum = sum + number
+            sum = sum * number
         return sum
 
         
